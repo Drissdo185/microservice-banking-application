44ed637ac38c3faec1be2d89af024d5f7ca94229
Đỗ Thành Đạt <dothanhdat185@gmail.com>
2025-02-10 15:16:13 +0700

    build impl

--- src/main/java/com/example/accounts/service/impl/AccountServiceImpl.java
+++ src/main/java/com/example/accounts/service/impl/AccountServiceImpl.java
@@ -1,11 +1,16 @@
 package com.example.accounts.service.impl;
 
+import com.example.accounts.constant.AccountsConstant;
 import com.example.accounts.dto.CustomerDto;
+import com.example.accounts.entity.Account;
+import com.example.accounts.entity.Customer;
 import com.example.accounts.repository.AccountRepository;
 import com.example.accounts.repository.CustomerRepository;
 import com.example.accounts.service.AccountService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -16,7 +21,21 @@ public class AccountServiceImpl implements AccountService {
 
 
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = new Customer();
+        Customer savedCustomer = customerRepository.save(customer);
+        accountRepository.save(createNewAccount(savedCustomer));
 
+    }
+
+    private Account createNewAccount(Customer customer) {
+        Account newAccount = new Account();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstant.SAVINGS);
+        newAccount.setBranchAddress("123 Hoc Mon");
+        return newAccount;
     }
 
 
